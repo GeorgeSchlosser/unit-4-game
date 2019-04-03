e46e86cac82c09e9155631dc1d0ce5246ab15bf5
Trash Panda <d.george.schlosser@gmail.com>
2019-04-03 02:00:40 -0400

    added variables and psuedocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,35 @@
-alert("test");
+// VARIABLES
+// ===================================================
+var wins = 0;
+var losses = 0;
+var numMatch;
+var score = 0; //start w/ 0 so that crystal values can be added on
+var c1Value;
+var c2Value;
+var c3Value;
+var c4Value;
+
+
+// FUNCTIONS
+// ===================================================
+
+// assign 1-12 to each of the 4 crystals
+
+// onclick add crystal value to score
+
+
+// MAIN PROCESSES
+// ===================================================
+
+// at game start assign value to numMatch and cxValue
+
+// when game ends reset numMatch, cxValue, and score
+
+if score === numMatch {
+    alert("YOU WIN!");
+    // reset function;
+}
+else if score > numMatch {
+    alert("YOU LOSE!");
+    //reset function
+}
