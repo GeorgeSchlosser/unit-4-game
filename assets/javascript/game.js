b6c6ce5237f3b253e45d1c233258686b819ea5a3
Trash Panda <d.george.schlosser@gmail.com>
2019-04-06 19:14:18 -0400

    notated game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,24 +8,25 @@ var c1Value;
 var c2Value;
 var c3Value;
 var c4Value;
-
+// cxValue represents the 4 above
 
 // FUNCTIONS
 // ===================================================
 
-function gameStart() {
+function gameStart() {                                              //start game and restart after first round
     score = 0;
     $("#num-score").text(score);
-    numMatch = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    numMatch = Math.floor(Math.random() * (120 - 19 + 1) + 19);     //pick score to match
     $("#num-to-match").text(numMatch);
-    c1Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    c1Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);         //pick value of crystal
     console.log(c1Value);
-    c2Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    c3Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    c4Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    c2Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);         //pick value of crystal
+    c3Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);         //pick value of crystal
+    c4Value = Math.floor(Math.random() * (12 - 1 + 1) + 1);         //pick value of crystal
     console.log(c2Value);
     console.log(c3Value);
     console.log(c4Value);
+    //link cxValue to corresponding div
     $("#c1").attr("value", c1Value);
     $("#c2").attr("value", c2Value);
     $("#c3").attr("value", c3Value);
@@ -35,7 +36,7 @@ function gameStart() {
 
 
 
-function roundOver() {
+function roundOver() {                 //win/lose parameters
     if (score === numMatch) {
         alert("YOU WIN!");
         wins = wins + 1;
@@ -50,7 +51,7 @@ function roundOver() {
     }
 }
 
-$(".crystal-btn").on("click", function(){
+$(".crystal-btn").on("click", function(){           //makes crystal images functional for gameplay
     var crystalValue = ($(this).attr("value"));
     crystalValue = parseInt(crystalValue);
     score += crystalValue;
